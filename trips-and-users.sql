868826138eab464870882a37f306fd8ed4e1ce7b
jamil.hossain <jamil.hossain@nexdecade.com>
2024-10-10 17:33:46 +0600

    trips-and-users-sql

--- trips-and-users.sql
+++ trips-and-users.sql
@@ -1,23 +1,14 @@
-WITH ValidTrips AS (
-    SELECT 
-        t.request_at,
-        t.status
-    FROM 
-        Trips t
-    JOIN 
-        Users u1 ON t.client_id = u1.users_id
-    JOIN 
-        Users u2 ON t.driver_id = u2.users_id
-    WHERE 
-        u1.banned = 'No' 
-        AND u2.banned = 'No'
-        AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-)
-SELECT 
-    request_at AS Day,
-    ROUND(SUM(CASE WHEN status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1 ELSE 0 END) / COUNT(*), 2) AS Cancellation_Rate
-FROM 
-    ValidTrips
-GROUP BY 
-    request_at;
+SELECT t.request_at AS Day,
+ROUND(SUM(CASE 
+            WHEN t.status = 'cancelled_by_driver' OR t.status = 'cancelled_by_client' 
+            THEN 1 ELSE 0 
+          END) / COUNT(*), 2) AS `Cancellation Rate`
+FROM Trips t
+LEFT JOIN Users u1 ON t.driver_id = u1.users_id
+LEFT JOIN Users u2 ON t.client_id = u2.users_id
+WHERE u1.banned = 'No'
+  AND u2.banned = 'No'
+  AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at
+ORDER BY t.request_at;
 
