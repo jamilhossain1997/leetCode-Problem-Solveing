8a72e96bdb795053f01fe87c5392ce22377ed2e3
jamil.hossain <jamil.hossain@nexdecade.com>
2024-10-12 15:06:56 +0600

    game-play-analysis-iv

--- game-play-analysis-iv.sql
+++ game-play-analysis-iv.sql
@@ -1,22 +1,13 @@
-WITH cte AS ( 
-
-    SELECT player_id,MIN(event_date) as first_login
+WITH cte AS (
+    SELECT player_id, MIN(event_date) AS first_login
     FROM Activity
-
-    Group by player_id
+    GROUP BY player_id
 )
 
-SELECT ROUND (SUM(
-
-    CASE 
-    WHEN DATEDIFF(event_date,first_login)=1
-    THEN 1
-    ELSE 0
-
-    END 
-) /COUNT(DISTINCT player_id),2
-) AS fraction
-
+SELECT ROUND(SUM(CASE 
+                    WHEN DATEDIFF(a.event_date, cte.first_login) = 1 
+                    THEN 1 
+                    ELSE 0 
+                 END) / COUNT(DISTINCT a.player_id), 2) AS fraction
 FROM Activity AS a
-
 JOIN cte ON a.player_id = cte.player_id;
