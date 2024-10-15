b55bf45604b2dc3182335e832ab3b76c6f2e3377
jamil.hossain <jamil.hossain@nexdecade.com>
2024-10-15 12:57:26 +0600

    palindromeNumber-11

--- Array/palindromeNumber.java
+++ Array/palindromeNumber.java
@@ -3,8 +3,16 @@ package Array;
 public class palindromeNumber {
 
     public static boolean Nums(int n) {
+
+        
+
+
         int reverse = 0;
         int temp = n;
+        if (temp < 0 || (temp != 0 && temp % 10 == 0)) {
+            return false;
+        }
+
         while (temp != 0) {
             reverse = (reverse * 10) + (temp % 10);
             temp = temp / 10;
@@ -15,7 +23,7 @@ public class palindromeNumber {
 
     public static void main(String[] args) {
         // Sample Input
-        int n = 122;
+        int n = 121;
         if (Nums(n)) {
             System.out.println("Yes");
         } else {
