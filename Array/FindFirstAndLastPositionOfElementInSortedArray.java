7ead04fb072ae224de42f9dde04c3f7be7f0cc4d
jamil.hossain <jamil.hossain@nexdecade.com>
2024-10-24 13:53:52 +0600

    FindFirstAndLast

---
+++ Array/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+   
+   public static void main(String [] args){
+      int [] nums={5,7,7,8,8,10};
+      int target=8;
+      System.out.println(Arrays.toString(searchRange(nums,target)));
+   }
+   
+   static int[] searchRange(int[] nums, int target) {
+        int[] arr = new int[2];
+        arr[0] = arr[1] = -1;
+
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                arr[0] = i; 
+                break;       
+            }
+        }
+
+        
+        if (arr[0] == -1) {
+            return arr;
+        }
+
+      
+        for (int i = arr[0]; i < nums.length; i++) {
+            if (nums[i] == target) {
+                arr[1] = i;  
+            }
+        }
+
+        return arr;
+    }
+}
