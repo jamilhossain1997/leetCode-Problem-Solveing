35e61de5249d3ac7edb4918cfbea8eb8796d509e
jamil.hossain <jamil.hossain@nexdecade.com>
2024-10-29 13:07:48 +0600

    FindThenIndex

---
+++ Array/FindTheIndexOfTheFirstOccurrenceString.java
@@ -0,0 +1,14 @@
+public class FindTheIndexOfTheFirstOccurrenceString {
+    public static  void main (String[] args){
+
+    }
+
+
+    static int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+             return 0;
+        }
+
+        int hLen=haystack.len
+    }
+}
